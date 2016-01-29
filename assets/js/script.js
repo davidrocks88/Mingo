73715b28f4fb9d0d675a608a87c1957dcd6a7314
David Bernstein <david.bernstein@tufts.edu>
2016-01-29 14:37:16 -0500

    Added parse database support

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
+Parse.initialize("rfxCYz4gizVzN9gRnQjcWigoHxKgxiWf33pAVVFY", "5M5b1gO1ioUC0OOBH0uUEmpICQWbHqaZ5Zs0Ix2D");
+
+var MingoWinnerName = Parse.Object.extend("MingoWinnerName");
+var newName = new MingoWinnerName();
+
 $(document).ready(function(){
         var usedArray = new Array(76);
-        var baseArray = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
         var sayings = new Array(
                 "Yes, yes, yes!", // 0
    		"*Censors himself*", // 1
@@ -39,6 +43,8 @@ $(document).ready(function(){
 			fillCard(i);
 		 }
                  mingo = false;
+                 var name = prompt("Please enter your name", "");
+                 newName.set("name", name);
 	 }
 	  	 
 	function fillCard(i){
@@ -89,42 +95,66 @@ $(document).ready(function(){
 
 		checkForMingo();
 	 });
+        function isMarked(cell) {
+                if(cell.attr('class')=="marked") {
+                        return true;
+                } else return false;
+        }
 
 	function checkForMingo() {
                 resetArrays();
-	 	// cols and rows
-	 	for(var i = 0; i < 5; i++) {
-			for(var j = 0; j < 5; j++) {
-				if($('#cell' + (j + 5 * i)).attr('class')=="marked") {
-	 				col[i]++;
-	 			}
-				if($('#cell' + (j * 5 + i)).attr('class')=="marked") {
-	 				row[i]++;
-	 			}
-			}
-	 	}
-	 	//diags
-	 	for(var i = 0; i < 5; i++) {
-			if($('#cell' + (i*6)).attr('class')=="marked") {
-	 			diag[0]++;
-                                console.log("diag[0]++");
-	 		}
-			if($('#cell' + (4+ i*4)).attr('class')=="marked") {
-	 			diag[1]++;
-                                console.log("diag[1]++");
-	 		}
-	 	}
-
+                checkRowsAndCols();
+                checkDiags();
                 if(!mingo) {
-        		for(var i = 0; i < 5; i++) {
-        	 		if(col[i] == 5 || row[i] == 5 || diag[0] == 5 || diag[1] == 5) {
-        	 			mingo = true;
-                                        alert("MINGO!");
-        	 			break;
-        	 		}
-        	 	}
+                        checkArrays();
                 }
+	}
 
-	 }
+        function checkRowsAndCols() {
+                for(var i = 0; i < 5; i++) {
+                        for(var j = 0; j < 5; j++) {
+                                if(isMarked($('#cell' + (j + 5 * i)))) {
+                                        col[i]++;
+                                }
+                                if(isMarked($('#cell' + (j * 5 + i)))) {
+                                        row[i]++;
+                                }
+                        }
+                }
+        }
+        function checkDiags() {
+                for(var i = 0; i < 5; i++) {
+                        if(isMarked($('#cell' + (i*6)))) {
+                                diag[0]++;
+                        }
+                        if(isMarked($('#cell' + (4+ i*4)))) {
+                                diag[1]++;
+                        }
+                }
+        }
+        function checkArrays() {
+                for(var i = 0; i < 5; i++) {
+                        if(col[i] == 5 || row[i] == 5 || 
+                          diag[0] == 5 || diag[1] == 5) {
+                                mingo = true;
+                                alert("MINGO!");
+                                saveNameInDb();
+                                break;
+                        }
+                }
+        }
+        function saveNameInDb() {
+                newName.save(null, {
+                        success: function(newName) {
+                        // Execute any logic that should take place after the object is saved.
+                        //alert('New object created with objectId: ' + newName.id);
+                },
+                error: function(newName, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and message.
+                    //alert('Failed to create new object, with error code: ' + error.message);
+                  }
+                });
+        }
  });
 
