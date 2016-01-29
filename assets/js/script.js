f64b192b046a628bd4fbc4835363d3bc98d646a8
David Bernstein <david.bernstein@tufts.edu>
2016-01-29 11:34:07 -0500

    Made code prettier, got rid of comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,34 @@
- $(document).ready(function(){
-   
-   var usedArray = new Array(76);
-   var baseArray = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
-   var sayings = new Array("Yes, yes, yes!", // 0
-   			   "*Censors himself*", // 1
-   			   "*Swears*", // 2
-   			   "*Awkwardly drinks water*", //3
-   			   "*Has uneven sleeves*", // 4
-   			   "What is this garbage?!", // 5
-   			   "Twitterland", //6
-   			   "Googleland", // 7
-   			   "Memes", // 8
-   			   "The internet is cat pictures", // 9
-   			   "Kneecaps blown out", // 10
-   			   "Sir Tim", // 11
-   			   "*Rick Rolls*", // 12
-   			   "*Simpsons picture*", // 13
-   			   "Appleland", // 14
-   			   "\"Bingo\"", // 15
-   			   "The answer is...", // 16
-   			   "Be more specific", // 17
-   			   "Do I know you from somewhere?", // 18
-   			   "4Chans" //19
-   			   );
-   // var number = 0;
+$(document).ready(function(){
+        var usedArray = new Array(76);
+        var baseArray = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+        var sayings = new Array("Yes, yes, yes!", // 0
+   		"*Censors himself*", // 1
+   		"*Swears*", // 2
+   		"*Awkwardly drinks water*", //3
+   		"*Has uneven sleeves*", // 4
+   		"What is this garbage?!", // 5
+   		"Twitterland", //6
+   		"Googleland", // 7
+   		"Memes", // 8
+   		"The internet is cat pictures", // 9
+   		"Kneecaps blown out", // 10
+   		"Sir Tim", // 11
+   		"*Rick Rolls*", // 12
+   		"*Simpsons picture*", // 13
+   		"Appleland", // 14
+   		"\"Bingo\"", // 15
+   		"The answer is...", // 16
+   		"Be more specific", // 17
+   		"Do I know you from somewhere?", // 18
+   		"4Chans" //19
+   		);
         var col = new Array(0,0,0,0,0);
         var row = new Array(0,0,0,0,0);
         var diag = new Array(0,0);
-   var number = "";
-   var base = 0;
+        var number = "";
         var mingo;
    
-   init();
+        init();
    
 	function init(){
 		 for(var i = 0; i<=24; i++){
@@ -40,9 +37,7 @@
                  mingo = false;
 	 }
 	  	 
-	 function fillCard(i){
-		 //base = baseArray[i] * 15;
-		 //number = base + Math.floor(Math.random()*15)+1;	
+	function fillCard(i){
                 number = sayings[Math.floor(Math.random() * 19)];
 	        $('#cell' + i).html(number);
                 if($('#cell' + i).class = "marked") {
@@ -55,7 +50,6 @@
                         }
                 }
 	 }
-	 
 	 
 	$('#newCard').click(function(){
         	resetArrays();
@@ -71,13 +65,13 @@
                 diag[1] = 0;
         }
 	 
-	 $('#newCard').click(function(){
-		resetArrays();
-		init();
-	 });
+	$('#newCard').click(function(){
+	       resetArrays();
+	       init();
+	});
 	 
-	 $('td').click(function(){
-		var toggle = this.style;
+	$('td').click(function(){
+	       var toggle = this.style;
                 if($(this).attr("class") == "unmarked") {
                         $(this).removeClass("unmarked");
 		        $(this).addClass("marked");
@@ -86,17 +80,13 @@
                         $(this).removeClass("marked");
                         $(this).addClass("unmarked");
                 }
-		//alert(this.class);
 		toggle.backgroundColor = toggle.backgroundColor? "":"#333";
 		toggle.color = toggle.color? "":"#fff";
-		//$(this).addClass("marked");
-		//alert($(this).attr('class'));
+
 		checkForMingo();
 	 });
 
-	 function checkForMingo() {
-
-
+	function checkForMingo() {
                 resetArrays();
 	 	// cols and rows
 	 	for(var i = 0; i < 5; i++) {
@@ -119,12 +109,7 @@
 	 			diag[1]++;
                                 console.log("diag[1]++");
 	 		}
-
 	 	}
-
-	 	// console.log(row);
-   //              console.log(col);
-   //              console.log(diag);
 
                 if(!mingo) {
         		for(var i = 0; i < 5; i++) {
@@ -137,6 +122,5 @@
                 }
 
 	 }
-
  });
 
