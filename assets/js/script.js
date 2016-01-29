f046e78425c389f43a5cca3a54b4a9504b05d8fd
David Bernstein <david.bernstein@tufts.edu>
2016-01-29 00:49:39 -0500

    Arrays will reset when get new card

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,11 @@
 		 //number = base + Math.floor(Math.random()*15)+1;	
 		 number = sayings[Math.floor(Math.random() * 19)];
 		 $('#cell' + i).html(number);
+                 if($('#cell' + i).class = "marked") {
+                        $('#cell' + i).removeClass("marked");
+                        $('#cell' + i).css("color","");
+                        $('#cell' + i).css("background-color","");
+                }
 
 
 		 // if(usedArray[number] != true){
@@ -74,7 +79,7 @@
         }
 	 
 	 $('#newCard').click(function(){
-		resetUsedNumbersArray();
+		resetArrays();
 		init();
 	 });
 	 
@@ -92,7 +97,7 @@
 	 function checkForMingo() {
 
 
-
+                resetArrays();
 	 	// cols and rows
 	 	for(var i = 0; i < 5; i++) {
 			for(var j = 0; j < 5; j++) {
@@ -108,14 +113,19 @@
 	 	for(var i = 0; i < 5; i++) {
 			if($('#cell' + (i*6)).attr('class')=="marked") {
 	 			diag[0]++;
+                                console.log("diag[0]++");
 	 		}
 			if($('#cell' + (4+ i*4)).attr('class')=="marked") {
 	 			diag[1]++;
+                                console.log("diag[1]++");
 	 		}
 
 	 	}
 
-	 	console.log(diag);
+	 	console.log(row);
+                console.log(col);
+                console.log(diag);
+
 
 		for(var i = 0; i < 5; i++) {
 	 		if(col[i] == 5 || row[i] == 5 || diag[0] == 5 || diag[1] == 5) {
